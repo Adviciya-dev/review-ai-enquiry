b527f3cb718dd538544d0f5486bc8ab3fa6086fc
aswin <aswinsatheesan45@gmail.com>
2025-10-11 19:52:14 +0530

    style: change input text color to white

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -5,97 +5,98 @@ import { useField } from "formik";
 import { Upload } from "lucide-react";
 
 interface Props {
-    label: string;
-    name: string;
-    type?: string;
-    placeholder?: string;
-    accept?: string;
-    multiple?: boolean;
+  label: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  accept?: string;
+  multiple?: boolean;
 }
 
-const FormInput: React.FC<Props> = ({ label, type = "text", multiple, accept, placeholder, name }) => {
-    const [field, meta, helpers] = useField(name);
+const FormInput: React.FC<Props> = ({
+  label,
+  type = "text",
+  multiple,
+  accept,
+  placeholder,
+  name,
+}) => {
+  const [field, meta, helpers] = useField(name);
 
-    const fileInputRef = React.useRef<HTMLInputElement>(null);
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
 
-    const isError = meta.touched && !!meta.error;
+  const isError = meta.touched && !!meta.error;
 
-    const handleFileClick = () => {
-        fileInputRef.current?.click();
-    };
+  const handleFileClick = () => {
+    fileInputRef.current?.click();
+  };
 
+  return (
+    <div className="flex flex-col">
+      <label className="block text-medium font-medium text-[#646060] mb-1">
+        {label}
+      </label>
 
+      {type === "file" ? (
+        <div className="relative">
+          <input
+            ref={fileInputRef}
+            id={name}
+            name={name}
+            type="file"
+            multiple={multiple}
+            accept={accept}
+            className="hidden"
+            suppressHydrationWarning
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              if (!e.target.files || e.target.files.length === 0) return;
 
+              if (multiple) {
+                const filesArray = Array.from(e.target.files);
+                helpers.setValue([...(field.value || []), ...filesArray]);
+              } else {
+                helpers.setValue(e.target.files[0]);
+              }
 
+              helpers.setTouched(true);
+            }}
+          />
 
-
-    return (
-        <div className="flex flex-col">
-            <label className="block text-medium font-medium text-[#646060] mb-1">{label}</label>
-
-            {type === "file" ? (
-                <div className="relative">
-
-                    <input
-                        ref={fileInputRef}
-                        id={name}
-                        name={name}
-                        type="file"
-                        multiple={multiple}
-                        accept={accept}
-                        className="hidden"
-                        suppressHydrationWarning
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            if (!e.target.files || e.target.files.length === 0) return;
-
-                            if (multiple) {
-                                const filesArray = Array.from(e.target.files);
-                                helpers.setValue([...(field.value || []), ...filesArray]);
-                            } else {
-                                helpers.setValue(e.target.files[0]);
-                            }
-
-                            helpers.setTouched(true);
-                        }}
-                    />
-
-
-                    <div
-                        onClick={handleFileClick}
-                        className={`flex items-center justify-between border rounded-md px-3 py-2 h-[42px] bg-white cursor-pointer hover:border-gray-400 transition ${isError
-                            ? "border-red-500"
-                            : "border-[#6F4444] "
-                            }`}
-                    >
-                        <span className="text-gray-700">
-                            {field.value
-                                ? Array.isArray(field.value)
-                                    ? `${field.value.length} files selected`
-                                    : field.value.name
-                                : "Choose file"}
-                        </span>
-                        <Upload className="w-5 h-5 text-gray-500 flex-shrink-0 ml-2" />
-                    </div>
-                </div>
-            ) : (
-                <input
-                    id={name}
-                    {...field}
-                    type={type}
-                    placeholder={placeholder}
-                    className={`
-                        w-full px-3 py-2 border border-[#6F4444] bg-black rounded-md h-[42px] focus:outline-none text-[16px] bold-400
-                         placeholder-[#646060] ${isError ? "border-red-500" : "border-[#6F4444] "
-                        }`}
-                    suppressHydrationWarning
-
-                />
-            )}
-
-            {/* Error message */}
-            {isError && <div className="text-red-500 text-sm mt-1">{meta.error}</div>}
+          <div
+            onClick={handleFileClick}
+            className={`flex items-center justify-between border rounded-md px-3 py-2 h-[42px] bg-white cursor-pointer hover:border-gray-400 transition ${
+              isError ? "border-red-500" : "border-[#6F4444] "
+            }`}
+          >
+            <span className="text-gray-700">
+              {field.value
+                ? Array.isArray(field.value)
+                  ? `${field.value.length} files selected`
+                  : field.value.name
+                : "Choose file"}
+            </span>
+            <Upload className="w-5 h-5 text-gray-500 flex-shrink-0 ml-2" />
+          </div>
         </div>
-    );
+      ) : (
+        <input
+          id={name}
+          {...field}
+          type={type}
+          placeholder={placeholder}
+          className={`
+                        w-full px-3 py-2 border border-[#6F4444] bg-black text-white rounded-md h-[42px] focus:outline-none text-[16px] bold-400
+                         placeholder-[#646060] ${
+                           isError ? "border-red-500" : "border-[#6F4444] "
+                         }`}
+          suppressHydrationWarning
+        />
+      )}
+
+      {/* Error message */}
+      {isError && <div className="text-red-500 text-sm mt-1">{meta.error}</div>}
+    </div>
+  );
 };
 
 export default FormInput;
